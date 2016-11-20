ee3253e9c8bc2aa771c9b6102dec253c5e2d338e
Willie Broder <willie.broder@gmail.com>
2016-11-20 21:39:39 +0000

    comment model added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
 
   validates :name, :description, :address, presence: true
   validates :name, length: { minimum: 4 } 
-
-
 end
