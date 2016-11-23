f7d255408caa687e62d9587f978991de7ea3ba5e
Willie Broder <willie.broder@gmail.com>
2016-11-23 04:34:44 +0000

    fully functional image uploader

--- db/migrate/20161123003455_create_photos.rb
+++ db/migrate/20161123003455_create_photos.rb
@@ -2,7 +2,6 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :caption
-      t.string :photo
 
       t.integer :place_id
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123003455) do
+ActiveRecord::Schema.define(version: 20161123033114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 20161123003455) do
 
   create_table "photos", force: true do |t|
     t.string   "caption"
-    t.string   "photo"
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
