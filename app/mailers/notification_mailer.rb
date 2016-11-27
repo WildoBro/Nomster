ee5ecca5cdc313323c16cd2bcd928ad4d408e86e
Willie Broder <willie.broder@gmail.com>
2016-11-27 20:04:08 +0000

    email functionlity established

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "willie.broder@gmail.com", 
+    mail(to: "williesmailers@gmail.com", 
       subject: "A comment has been added to your place")
   end
 
